94258b8221ba94fcb628ba08ca87c7bb4a529160
lconowalov4 <rw6lfe@gmail.com>
2025-09-29 22:59:06 +0300

    Adding axios to a project

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
 import Card from "./Card";
 import Pokeinfo from "./Pokeinfo";
+import axios from "axios";
 
 const Main = () => {
+    const [pokeData,setPokeData] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const [url,setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+
+    const pokeFun = async() => {
+        setLoading(true)
+        const res = await axios.get(url);
+        
+    }
+
+    useEffect(() => {
+        pokeFun();
+    }, [url])
     return(
         <>
             <div className="container">
                 <div className="left-content">
-                    <Card/>
-                    <Card/>
                     <Card/>
                     <Card/>
                     <Card/>
